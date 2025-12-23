b376af79298258dfbf87dbdfabf7eddda4b8eb61
Vendrim <vbialek@cbialek.de>
2025-12-23 15:03:15 +0100

    add react router

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import Home from "./pages/Home";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/device/all")
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }, []);
+
+  return (
+    <BrowserRouter>
+      {/* Navigation */}
+      <nav className="menu-bar">
+        <Link to="/">
+          <span>Cat Tracker</span>
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,30 @@
+import { LatLngExpression } from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+export default function Home() {
+  const position: LatLngExpression = [37.0353607, -7.8377805];
+
+  return (
+    <div className="main-container">
+      <div className="content-container">
+        <div className="content-card">
+          <span>Here you can find/search your cat:</span>
+        </div>
+      </div>
+      <MapContainer
+        center={position}
+        zoom={13}
+        style={{ height: "400px", width: "100%" }}
+      >
+        <TileLayer
+          attribution="© OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={position}>
+          <Popup>Marker text</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
