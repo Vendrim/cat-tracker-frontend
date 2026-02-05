0012fc7255eeba6fc760b857c0d10d0a79cb09fa
ginseng101 <ginseng101@proton.me>
2026-02-05 17:37:17 +0100

    mod: Kommunikation mit Backend. Bilder lassen sich uploaden. Alles funktioniert.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import "./App.css";
 import Home from "./pages/Home";
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ImageUpload from "./ImageUplaoad";
 
 function App() {
   useEffect(() => {
-    fetch("http://localhost:8080/api/v1/device/all")
+    fetch("http://localhost:8082/api/v1/device/all")
       .then((res) => res.json())
       .then((json) => console.log(json));
   }, []);
@@ -21,6 +22,10 @@ function App() {
           <img src="src/assets/pfp.png"></img>
         </div>
       </nav>
+       <div className="image-upload">
+        <ImageUpload />
+       </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/ImageUplaoad.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function ImageUpload() {
+    const [file, setFile] = useState(null);
+    const [imageUrl, setImageUrl] = useState(null);
+
+    const uploadImage = async () => {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const res = await fetch("http://localhost:8082/api/images/upload", {
+            method: "POST",
+            body: formData
+        });
+
+        const filename = await res.text();
+
+        setImageUrl(`http://localhost:8082/api/images/${filename}`);
+    };
+
+    return (
+        <div>
+            <input
+                type="file"
+                onChange={e => setFile(e.target.files[0])}
+            />
+
+            <button onClick={uploadImage}>
+                Upload
+            </button>
+
+            {imageUrl && (
+                <img
+                    src={imageUrl}
+                    alt="Uploaded"
+                    style={{ width: "300px", marginTop: "20px" }}
+                />
+            )}
+        </div>
+    );
+}
+
+export default ImageUpload;
