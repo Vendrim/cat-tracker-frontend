3f85c9531102b208acb27224bcc378114b9262d5
Victor Bialek <vbialek@cbialek.de>
2026-02-07 18:47:18 +0100

    xdbdf sdf

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,44 +1,42 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 function ImageUpload() {
-    const [file, setFile] = useState(null);
-    const [imageUrl, setImageUrl] = useState(null);
+    const [file, setFile] = useState(null)
+    const [imageUrl, setImageUrl] = useState(null)
 
     // @ts-ignore
     const uploadImage = async () => {
-        const formData = new FormData();
-        formData.append("file", file);
+        const formData = new FormData()
+        formData.append('file', file)
 
-        const res = await fetch(import.meta.env.VITE_BACKEND_URL+"api/images/upload", {
-            method: "POST",
-            body: formData
-        });
+        const res = await fetch(
+            `${import.meta.env.VITE_BACKEND_URL}api/images/upload`,
+            {
+                method: 'POST',
+                body: formData,
+            }
+        )
 
-        const filename = await res.text();
+        const filename = await res.text()
 
         setImageUrl(`${import.meta.env.VITE_BACKEND_URL}api/images/${filename}`)
-    };
+    }
 
     return (
         <div>
-            <input
-                type="file"
-                onChange={e => setFile(e.target.files[0])}
-            />
+            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
 
-            <button onClick={uploadImage}>
-                Upload
-            </button>
+            <button onClick={uploadImage}>Upload</button>
 
             {imageUrl && (
                 <img
                     src={imageUrl}
                     alt="Uploaded"
-                    style={{ width: "300px", marginTop: "20px" }}
+                    style={{ width: '300px', marginTop: '20px' }}
                 />
             )}
         </div>
-    );
+    )
 }
 
-export default ImageUpload;
+export default ImageUpload
