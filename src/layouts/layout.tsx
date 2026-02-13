29f74f53fe9bf634b1af783505eb9d9dfe004984
Victor Bialek <victor.bialek@cstx.de>
2026-02-13 15:02:07 +0100

    fix the problem we had with the import meta env imports. Let the typescript server know, everything is alright

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,18 +1,17 @@
-import { Link } from "react-router-dom";
-import React from "react";
+import { Link } from 'react-router-dom'
 
 export default function Layout(props: { children: React.JSX.Element }) {
-  return (
-    <>
-      <nav className="menu-bar">
-        <Link to="/">
-          <span>Cat Tracker</span>
-        </Link>
-        <div className="profile-img">
-          <img src="src/assets/pfp.png"></img>
-        </div>
-      </nav>
-      {props.children}
-    </>
-  );
+    return (
+        <>
+            <nav className="menu-bar">
+                <Link to="/home">
+                    <span>Cat Tracker</span>
+                </Link>
+                <div className="profile-img">
+                    <img src="src/assets/pfp.png"></img>
+                </div>
+            </nav>
+            {props.children}
+        </>
+    )
 }
